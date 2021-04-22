d492b2f160e75af778a3ec542bc1072e4e2f268c
qujinwei <qujinwei@tal.com>
2021-04-22 23:00:26 +0800

    1、代码分离 2、环境配置

---
+++ dist/print111.bundle.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["print111"],{
+
+/***/ "./src/lib/math.js":
+/*!*************************!*\
+  !*** ./src/lib/math.js ***!
+  \*************************/
+/*! exports provided: square, cube, cla */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"square\", function() { return square; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"cube\", function() { return cube; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"cla\", function() { return cla; });\nfunction square(x = {}) {\n    console.log(11111)\n    x.a = 1\n    x.b = 2\n    cube()\n}\n\nlet obj = {}\nfunction cube(x) {\n    console.log(22222)\n    obj.a = 'abc'\n    window.asdf = 43\n    return x * x * x;\n}\n\nclass cla {\n    constructor() {\n        console.log(33333)\n    }\n}\n\n//# sourceURL=webpack:///./src/lib/math.js?");
+
+/***/ })
+
+}]);
